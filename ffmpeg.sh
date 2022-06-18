49d909d26eb2a1f89755d1b5bcb97f6ea6eb27fb
Musify <84908356+Musify1@users.noreply.github.com>
2022-06-18 15:20:09 +0530

    Update ffmpeg.sh

--- ffmpeg.sh
+++ ffmpeg.sh
@@ -9,11 +9,11 @@ FPS="30"                                       # FPS de la vidéo en sortie
 QUAL="ultrafast"                                  # Preset de qualité FFMPEG
 YOUTUBE_URL="rtmp://x.rtmp.youtube.com/live2"  # URL de base RTMP youtube
 
-SOURCE="video.mp4"              # Source UDP (voir les annonces SAP)
-KEY="$KEY"                                     # Clé à récupérer sur l'event youtube
+SOURCE="yt5s.com-møndberg - Living Free�� [lofi hip hop_relaxing beats](360p).mp4"              # Source UDP (voir les annonces SAP)
+KEY="pz4t-jpqh-z0xx-04j5-9x3j"                                     # Clé à récupérer sur l'event youtube
 
 ffmpeg \
     -stream_loop -1 -i "$SOURCE" -deinterlace \
     -vcodec libx264 -pix_fmt yuv420p -preset $QUAL -r $FPS -g $(($FPS * 2)) -b:v $VBR \
     -acodec libmp3lame -ar 44100 -threads 6 -qscale 3 -b:a 712000 -bufsize 512k \
-    -f flv "$YOUTUBE_URL/$KEY"
+    -f flv "$YOUTUBE_URL/pz4t-jpqh-z0xx-04j5-9x3j"
